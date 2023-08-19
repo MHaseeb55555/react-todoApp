442e7b945a41b70ff43866697dc7340fdb00a229
MHaseeb55555 <m83243780@gmail.com>
2023-08-19 17:12:48 +0500

    work complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+// import image1 from '../assets./image.avif';
+import { useState } from 'react'
 import './App.css';
 
 function App() {
+
+  // //================ TODO APP =================
+
+  let [text, setText] = useState('');
+  let [list, setList] = useState([]);
+
+  let update = () => {
+    list.push(text);
+    setList([...list]);
+  }
+
+
+  let edit = () => {
+    let update = prompt('ENTER NEW VALUE')
+
+  }
+
+  let dlt = (x) => {
+    x.value = '';
+
+
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{textAlign:'center'}}>
+
+      <h1 style={{background:'blue',color:'wheat'}}>TODO APP</h1>
+
+      <input onChange={(e) => setText(e.target.value)} />
+      <button onClick={update}>ADD</button>
+
+      {list.map((x, i) => {
+
+
+        return (
+          <div>
+            <p key={i}>  {i + 1}. {x} <button onClick={dlt}>DELETE</button> <button onClick={edit}>EDIT</button> </p>
+
+
+
+          </div>
+
+        )
+      })}
+
+
+
+
+
     </div>
   );
+
+
 }
 
 export default App;
